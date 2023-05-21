612f8bda2100f06f75203ee339217f0ec777a93e
AndyNoob <comfortableandy9082@email.com>
2023-05-21 00:17:06 -0700

    Initial commit

---
+++ src/main/java/me/comfortable_andy/thathurts/ThatHurtsMain.java
@@ -0,0 +1,17 @@
+package me.comfortable_andy.thathurts;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class ThatHurtsMain extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
